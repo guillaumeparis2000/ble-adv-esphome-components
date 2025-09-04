549570a95255a349fc7d37c629923e556e146783
Guillaume PARIS <guillaume.paris@telefonica.com>
2025-09-04 13:19:38 +0200

    Remove internal registration calls from BleAdvSelect and BleAdvNumber sub_init methods

--- components/ble_adv_handler/ble_adv_handler.cpp
+++ components/ble_adv_handler/ble_adv_handler.cpp
@@ -563,7 +563,6 @@ void BleAdvSelect::control(const std::string &value) {
 }
 
 void BleAdvSelect::sub_init() {
-  App.register_select_internal(this);
   this->rtc_ = global_preferences->make_preference<uint32_t>(this->get_object_id_hash());
   uint32_t restored;
   if (this->rtc_.load(&restored)) {
@@ -582,7 +581,6 @@ void BleAdvNumber::control(float value) {
 }
 
 void BleAdvNumber::sub_init() {
-  App.register_number_internal(this);
   this->rtc_ = global_preferences->make_preference<float>(this->get_object_id_hash());
   float restored;
   if (this->rtc_.load(&restored)) {
